86843a451baeba0bb5671570bab045e77f03b5fc
Lenore <52559898+Ltah72@users.noreply.github.com>
2026-04-29 11:30:15 +1200

    Update PercentChanges_F2vF0.R
    Making it more reproducible/translatable for public usage

--- neuropsych/PercentChanges_F2vF0.R
+++ neuropsych/PercentChanges_F2vF0.R
@@ -1,3 +1,5 @@
+#calculate and visualise the percent changes in the neuropsychogical variables from baseline (F0) to a time period (e.g., F2, two years later), for longitudinal analysis 
+
 #Author: Lenore Tahara-Eckl
 #Email: Ltah262@aucklanduni.ac.nz
 #Date: 20/06/21
@@ -9,79 +11,91 @@ pacman::p_load(dplyr, ggplot2, psych, car, multcomp, lsr, BayesFactor, tidyr, GP
 source("https://raw.githubusercontent.com/datavizpyr/data/master/half_flat_violinplot.R") #for raincloud graph
 
 #set up pathway
-#setwd('/yourpathway/')
+setwd('/yourpathway/')
 #file.choose function
-setwd('H:/ltah262/PhD/ExecutiveFunction/NeuroPsychAssessment/data/')
+#e.g., setwd('H:/ltah262/PhD/ExecutiveFunction/NeuroPsychAssessment/data/')
 
 
 #######----------------- Longitudinal (F0 vs. F2) analysis -----------------########
 
 #read in csv files (participant file)
-DPRC_neuropsych_data <- read.csv("longitudinal_DPRC_neuropsych_data_lined_up_valid_participants.csv")
+your_data_file <- read.csv("your_longitudinal_data_file.csv")
+#e.g, DPRC_neuropsych_data <- read.csv("longitudinal_DPRC_neuropsych_data_lined_up_valid_participants.csv")
 
 #rename first column 
-colnames(DPRC_neuropsych_data)[1] <-'ParticipantID'
+colnames(your_data_file)[1] <-'ParticipantID'
 
 #convert variables
-DPRC_neuropsych_data$Group <- as.factor(DPRC_neuropsych_data$Group)
-DPRC_neuropsych_data$Sex_binary <- as.factor(DPRC_neuropsych_data$Sex_binary)
-DPRC_neuropsych_data$Timepoint <- as.factor(DPRC_neuropsych_data$Timepoint)
-
+your_data_file$Group <- as.factor(your_data_file$Group)
+your_data_file$Sex_binary <- as.factor(your_data_file$Sex_binary)
+your_data_file$Timepoint <- as.factor(your_data_file$Timepoint)
 
 #Percentage changes ((F2 - F0) / F0)
 #put into long format
-percent_change_data <- DPRC_neuropsych_data %>% 
+percent_change_data <- your_data_file %>% 
   filter(Timepoint == "F2") %>%
   select(ParticipantID, 
          Age,
-         Classification,
          Group, 
-         Sex,
          Sex_binary,
          Timepoint,
-         PercentChange_TMTA,
-         PercentChange_TMTB,
-         PercentChange_ColorNaming,
-         PercentChange_WordReading,
-         PercentChange_Inhibition,
-         PercentChange_LetFluency,
-         PercentChange_CatFluency,
-         PercentChange_Switching,
-         PercentChange_HayBTime1z,
-         PercentChange_HayBTime2z,
-         PercentChange_HayBCatAz,
-         PercentChange_HayBCatBz)
+         PercentChange_variable1,
+         PercentChange_variable2,
+         etc...)
+#example below:
+#percent_change_data <- DPRC_neuropsych_data %>% 
+ # filter(Timepoint == "F2") %>%
+ # select(ParticipantID, 
+  #       Age,
+   #      Classification,
+    #     Group, 
+     #    Sex,
+      #   Sex_binary,
+       #  Timepoint,
+        # PercentChange_TMTA,
+         #PercentChange_TMTB,
+         #PercentChange_ColorNaming,
+         #PercentChange_WordReading,
+         #PercentChange_Inhibition,
+         #PercentChange_LetFluency,
+         #PercentChange_CatFluency,
+         #PercentChange_Switching,
+         #PercentChange_HayBTime1z,
+         #PercentChange_HayBTime2z,
+         #PercentChange_HayBCatAz,
+         #PercentChange_HayBCatBz)
 
 #rename variables 
-percent_change_data <- rename(percent_change_data, TMTA = PercentChange_TMTA)
-percent_change_data <- rename(percent_change_data, TMTB = PercentChange_TMTB)
-percent_change_data <- rename(percent_change_data, ColorNaming = PercentChange_ColorNaming)
-percent_change_data <- rename(percent_change_data, WordReading = PercentChange_WordReading)
-percent_change_data <- rename(percent_change_data, Inhibition = PercentChange_Inhibition)
-percent_change_data <- rename(percent_change_data, LetFluency = PercentChange_LetFluency)
-percent_change_data <- rename(percent_change_data, CatFluency = PercentChange_CatFluency)
-percent_change_data <- rename(percent_change_data, Switching = PercentChange_Switching)
-percent_change_data <- rename(percent_change_data, HayBTime1z = PercentChange_HayBTime1z)
-percent_change_data <- rename(percent_change_data, HayBTime2z = PercentChange_HayBTime2z)
-percent_change_data <- rename(percent_change_data, HayBCatAz = PercentChange_HayBCatAz)
-percent_change_data <- rename(percent_change_data, HayBCatBz = PercentChange_HayBCatBz)
+percent_change_data <- rename(percent_change_data, variable_name = PercentChange_variable_name)
+
+#examples below:
+#percent_change_data <- rename(percent_change_data, TMTA = PercentChange_TMTA)
+#percent_change_data <- rename(percent_change_data, ColorNaming = PercentChange_ColorNaming)
+#...
 
 #put into long format
 percent_change_data_long <- gather(percent_change_data, 
                                    "Test",
                                    "Percent_change", 
-                                   TMTA,
-                                   TMTB,
-                                   ColorNaming,
-                                   WordReading,
-                                   Inhibition,
-                                   LetFluency,
-                                   CatFluency,
-                                   Switching,
-                                   HayBTime1z,
-                                   HayBTime2z,
-                                   HayBCatAz,
-                                   HayBCatBz)
+                                   variable1,
+                                   variable2,
+                                   etc...)
+#example below:
+#percent_change_data_long <- gather(percent_change_data, 
+ #                                  "Test",
+  #                                 "Percent_change", 
+   #                                TMTA,
+    #                               TMTB,
+     #                              ColorNaming,
+      #                             WordReading,
+       #                            Inhibition,
+        #                           LetFluency,
+         #                          CatFluency,
+          #                         Switching,
+           #                        HayBTime1z,
+            #                       HayBTime2z,
+             #                      HayBCatAz,
+              #                     HayBCatBz)
 #plot percent changes (whole)
 ggplot(percent_change_data_long, aes(x = Test, y = Percent_change)) + 
   geom_point(aes(y = Percent_change, color = Percent_change), position = position_jitter(width = .15), size = .5, alpha = 0.8) +
@@ -91,83 +105,23 @@ ggplot(percent_change_data_long, aes(x = Test, y = Percent_change)) +
   theme_classic()+
   theme(legend.position = "none")
 
-
-#put into long format (no Hayling)
-percent_change_data_noHay_long <- gather(percent_change_data, 
-                                   "Test",
-                                   "Percent_change", 
-                                   TMTA,
-                                   TMTB,
-                                   ColorNaming,
-                                   WordReading,
-                                   Inhibition,
-                                   LetFluency,
-                                   CatFluency,
-                                   Switching)
-#plot percent changes (no Hayling)
-ggplot(percent_change_data_noHay_long, aes(x = Test, y = Percent_change)) + 
-  geom_point(aes(y = Percent_change, color = Percent_change), position = position_jitter(width = .15), size = .5, alpha = 0.8) +
-  #geom_boxplot(width = 0.1, fill = "white", outlier.size = .15, aes(colour = Percent_change)) + 
-  xlab("Tests") + 
-  ylab("Percent Change") +
-  theme_classic() +
-  theme(legend.position = "none")
-                               
-#put into long format (only Hayling)
-percent_change_Hay_long <- gather(percent_change_data, 
-                                   "Test",
-                                   "Percent_change", 
-                                   HayBTime1z,
-                                   HayBTime2z,
-                                   HayBCatAz,
-                                   HayBCatBz)
-
-#plot percent changes (only Hayling)
-ggplot(percent_change_Hay_long, aes(x = Test, y = Percent_change)) + 
-  geom_point(aes(y = Percent_change, color = Percent_change), position = position_jitter(width = .15), size = .5, alpha = 0.8) +
-  #geom_boxplot(width = 0.1, fill = "white", outlier.size = .15, aes(colour = Percent_change)) + 
-  xlab("Hayling's Sentence Completion Test") + 
-  ylab("Percent Change") +
-  theme_classic() +
-  theme(legend.position = "none")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#plot percent changes (no Hayling, only aMCI)
-ggplot(subset(percent_change_data_noHay_long, Group %in% ("3")))+ 
-  geom_point(aes(x = Test, y = Percent_change, color = Percent_change), position = position_jitter(width = .15), size = .5, alpha = 0.8) +
+#Example: plot percent changes for one participant (no Hayling, only aMCI)
+#ggplot(subset(percent_change_data_noHay_long, Group %in% ("3")))+ 
+ # geom_point(aes(x = Test, y = Percent_change, color = Percent_change), position = position_jitter(width = .15), size = .5, alpha = 0.8) +
   #geom_text() +
   #geom_boxplot(width = 0.1, fill = "white", outlier.size = .15, aes(colour = Percent_change)) + 
-  xlab("Tests") + 
-  ylab("Percent Change") +
-  theme_classic() +
-  theme(legend.position = "none")
+  #xlab("Tests") + 
+  #ylab("Percent Change") +
+  #theme_classic() +
+  #theme(legend.position = "none")
 
-
-
-
-#plot by participant percent changes (no Hayling)
-ggplot(percent_change_data_noHay_long, aes(x = ParticipantID, y = Percent_change)) + 
-  geom_point(aes(y = Percent_change, color = Percent_change), position = position_jitter(width = .15), size = .5, alpha = 0.8) +
+#Example: plot by participant percent changes (no Hayling)
+#ggplot(percent_change_data_noHay_long, aes(x = ParticipantID, y = Percent_change)) + 
+ # geom_point(aes(y = Percent_change, color = Percent_change), position = position_jitter(width = .15), size = .5, alpha = 0.8) +
   #geom_boxplot(width = 0.1, fill = "white", outlier.size = .15, aes(colour = Percent_change)) + 
-  xlab("Participant") + 
-  ylab("Percent Change") +
-  theme_classic() +
-  theme(legend.position = "none")
+  #xlab("Participant") + 
+  #ylab("Percent Change") +
+  #theme_classic() +
+  #theme(legend.position = "none")
 
 
