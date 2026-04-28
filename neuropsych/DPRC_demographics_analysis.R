b69970aa802e8632b2e1fcb264a4ebe31f19393e
Lenore <52559898+Ltah72@users.noreply.github.com>
2026-04-28 21:53:27 +1200

    Update DPRC_demographics_analysis.R
    Updating/cleaning code for public look

--- neuropsych/DPRC_demographics_analysis.R
+++ neuropsych/DPRC_demographics_analysis.R
@@ -3,11 +3,9 @@
 #participant groups of interest, which are: Controls (C), subjective cognitive decline (SCD), amnestic mild cognitive 
 #impairment (aMCI), multiple-domain mild cognitive impairment (mMCI), and Alzheimer's disease (AD).   
 
-
 #Author: Lenore Tahara-Eckl
 #Email: Ltah262@aucklanduni.ac.nz
 #Date: 1/12/20
-
 
 #load libraries via pacman
 pacman::p_load(dplyr, ggplot2, psych, car, lsr, BayesFactor, RColorBrewer)
@@ -24,26 +22,25 @@ setwd('/yourpathway/')
 #e.g., setwd('H:/ltah262/PhD/ExecutiveFunction/NeuroPsychAssessment/data/')
 
 
-
 #######----------------- Cross-sectional (F0) analysis -----------------########
 
 #read in excel files (participant file) - choose cross-sectional or longitudinal analysis
 your_file_name <- read.csv("your_file_name.csv") #cross-sectional
-#e.g., DPRC_demographics <- read.csv("cross-sectional_DPRC_neuropsych_data_lined_up_valid_participants.csv")
+#e.g., your_file_name <- read.csv("cross-sectional_DPRC_neuropsych_data_lined_up_valid_participants.csv")
 
 #rename first column 
-colnames(DPRC_demographics)[1] <-'ParticipantID'
+colnames(your_file_name)[1] <-'ParticipantID'
 
 #convert categorical variables to a factor
-DPRC_demographics$Group <- as.factor(DPRC_demographics$Group)
-DPRC_demographics$Sex_binary <- as.factor(DPRC_demographics$Sex_binary)
+your_file_name$Group <- as.factor(your_file_name$Group)
+your_file_name$Sex_binary <- as.factor(your_file_name$Sex_binary)
 
 #convert variables to numeric 
-DPRC_demographics$Age<- as.numeric(DPRC_demographics$Age)
-DPRC_demographics$ACE<- as.numeric(DPRC_demographics$ACE)
+your_file_name$Age<- as.numeric(your_file_name$Age)
+your_file_name$ACE<- as.numeric(your_file_name$ACE)
 
 #add in trend Group variable
-Trend_Group <- as.numeric(DPRC_demographics$Group)
+Trend_Group <- as.numeric(your_file_name$Group)
 #Trend group with contrasts that sum to zero
 Trend_Group_equate_zero_contrast<- vector(mode='numeric',length=length(Trend_Group))
 for (i in seq(Trend_Group)) {
@@ -61,29 +58,26 @@ for (i in seq(Trend_Group)) {
 }
 
 #look at descriptive statistics
-age_descrip <- describeBy(DPRC_demographics$Age, DPRC_demographics$Group)
-ACE_descrip <- describeBy(DPRC_demographics$ACE, DPRC_demographics$Group)
-gender_descrip <- by(DPRC_demographics$Group, DPRC_demographics$Sex, summary)
-gender_descrip_detail <- describeBy(DPRC_demographics ~ Sex_binary + Group, skew=FALSE, ranges=FALSE)
-clinsite_descrip <- by(DPRC_demographics$Group, DPRC_demographics$Clinical_site, summary)
-
-#clinsite_descrip <- describeBy(DPRC_demographics ~ Sex_binary + Group, skew=FALSE, ranges=FALSE)
+age_descrip <- describeBy(your_file_name$Age, your_file_name$Group)
+ACE_descrip <- describeBy(your_file_name$ACE, your_file_name$Group)
+gender_descrip <- by(your_file_name$Group, your_file_name$Sex, summary)
+gender_descrip_detail <- describeBy(your_file_name ~ Sex_binary + Group, skew=FALSE, ranges=FALSE)
+clinsite_descrip <- by(your_file_name$Group, your_file_name$Clinical_site, summary)
 
 #find mean & SD from total sample:
 #Age
-mean(DPRC_demographics$Age)
-sd(DPRC_demographics$Age)
+mean(your_file_name$Age)
+sd(your_file_name$Age)
 #ACE
-all_ACE <- DPRC_demographics$ACE
+all_ACE <- your_file_name$ACE
 noNAsACE <- na.omit(all_ACE)
 mean(noNAsACE)
 sd(noNAsACE)
 
-
 ####---------------------plot & analyse the data to visualise-------------------------------#####
-#Examples of plotting data
+#Examples of plotting data with different types of plots
 #plot age (as a violin plot)
-ggplot(DPRC_demographics, aes(x = Group, y = Age)) + 
+ggplot(your_file_name, aes(x = Group, y = Age)) + 
     geom_boxplot(width = 0.1, fill = "white", outlier.size = 1, aes(colour = Group)) + 
     stat_summary(fun = mean, geom = "point", shape = 19, size = 2, aes(colour = Group)) + 
     ylim(50, 95) +
@@ -95,7 +89,7 @@ ggplot(DPRC_demographics, aes(x = Group, y = Age)) +
     geom_violin(trim = FALSE, alpha = .5, aes(fill = Group, colour = Group), size = 1)
 
 #plot age (as a raincloud plot)
-ggplot(DPRC_demographics, aes(x = Group, y = Age, fill = Group)) + 
+ggplot(your_file_name, aes(x = Group, y = Age, fill = Group)) + 
     geom_flat_violin(position = position_nudge(x = .2, y = 0), alpha = .8) +
     geom_point(aes(y = Age, color = Group), position = position_jitter(width = .15), size = .5, alpha = 0.8) +
     geom_boxplot(width = 0.1, fill = "white", outlier.size = 1, aes(colour = Group)) + 
@@ -109,7 +103,7 @@ ggplot(DPRC_demographics, aes(x = Group, y = Age, fill = Group)) +
     coord_flip()
 
 #plot gender
-gender_data <- data.frame(table(DPRC_demographics$Classification, DPRC_demographics$Sex))
+gender_data <- data.frame(table(your_file_name$Classification, your_file_name$Sex))
 names(gender_data) <- c("Group", "Sex", "Count")
 Group_order <- c("C", "SCD", "aMCI", "mMCI", "AD")
 gender_data <- gender_data %>% arrange(factor(Group, levels=Group_order))
@@ -121,22 +115,22 @@ ggplot(data=gender_data, aes(x=Group, y=Count, fill=Sex)) +
     theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
 
 #linear trend example plot
-#DPRC_demographics$Group <- as.numeric(DPRC_demographics$Group)
-ggplot(DPRC_demographics, aes(x = Group, y = ACE, fill = Group)) + 
+#your_file_name$Group <- as.numeric(your_file_name$Group)
+ggplot(your_file_name, aes(x = Group, y = ACE, fill = Group)) + 
     geom_point(aes(y = ACE, color = factor(Group))) +
     geom_smooth(method = lm, se = TRUE)+ 
     xlab("Group") + 
     ylab("ACE") +
     theme_classic() +
     theme(legend.position = "none")
-#DPRC_demographics$Group <- as.factor(DPRC_demographics$Group)
+#your_file_name$Group <- as.factor(your_file_name$Group)
 
 #check for significant difference in age between groups example
-age_mod <- lm(Age ~ Classification, data = DPRC_demographics)
-#age_mod <- lm(Age ~ 0 + Classification, data = DPRC_demographics) #test against y-intercept
+age_mod <- lm(Age ~ Classification, data = your_file_name)
+#age_mod <- lm(Age ~ 0 + Classification, data = your_file_name) #test against y-intercept
 anova(age_mod)
 #Bayesian (put into a new dataset b/c can't have any NA values)
-For_Bay_data <- dplyr::select(DPRC_demographics, ParticipantID, Group, Age, ACE)
+For_Bay_data <- dplyr::select(your_file_name, ParticipantID, Group, Age, ACE)
 summary(For_Bay_data)
 For_Bay_data_noNas <- na.omit(For_Bay_data)
 anovaBF(Age ~ Group, data = For_Bay_data_noNas) 
@@ -147,7 +141,6 @@ etaSquared(age_mod)
 age_group_means <- c(age_descrip$`1`$mean, age_descrip$`2`$mean, age_descrip$`3`$mean, age_descrip$`4`$mean, age_descrip$`5`$mean)
 power_age_n <- power.anova.test(groups = length(age_group_means), between.var = anova(age_mod)$`Sum Sq`[1], within.var = anova(age_mod)$`Sum Sq`[2], power = .8, sig.level = 0.05)
 power_age_power<- power.anova.test(groups = length(ACE_group_means), between.var = anova(age_mod)$`Sum Sq`[1], within.var = anova(age_mod)$`Sum Sq`[2], n = 41, sig.level = 0.05)
-
 
 #check for significant difference in gender between groups example. 
 #reformat data for chi-square test
@@ -162,9 +155,9 @@ contingencyTableBF(gender_data_chisq, sampleType = "jointMulti")
 cramersV(gender_data_chisq)
 
 #check for homogeneity of variance, example with ACE variable 
-leveneTest(ACE~Group, data=DPRC_demographics) #violation
+leveneTest(ACE~Group, data=your_file_name) #violation
 #check for significant difference in ACE between groups 
-ACE_mod <- lm(ACE ~ Group, data = DPRC_demographics)
+ACE_mod <- lm(ACE ~ Group, data = your_file_name)
 anova(ACE_mod)
 summary(ACE_mod) #for linear trend analysis
 anovaBF(ACE ~ Group, data = For_Bay_data_noNas) 
@@ -177,19 +170,19 @@ power_ACE_n <- power.anova.test(groups = length(ACE_group_means), between.var = 
 power_ACE_power<- power.anova.test(groups = length(ACE_group_means), between.var = anova(ACE_mod)$`Sum Sq`[1], within.var = anova(ACE_mod)$`Sum Sq`[2], n = 41, sig.level = 0.05)
 
 #add in Linear Trend Analysis in Linear Regression
-ACE_LinTrend_mod <- lm(ACE ~ Trend_Group + Group, data = DPRC_demographics)
+ACE_LinTrend_mod <- lm(ACE ~ Trend_Group + Group, data = your_file_name)
 anova(ACE_LinTrend_mod)
 summary(ACE_LinTrend_mod) 
 
 #Try with the linear contrasts summing to zero
-ACE_LinTrendZeroCont_mod <- lm(ACE ~ Trend_Group_equate_zero_contrast + Group, data = DPRC_demographics)
+ACE_LinTrendZeroCont_mod <- lm(ACE ~ Trend_Group_equate_zero_contrast + Group, data = your_file_name)
 anova(ACE_LinTrendZeroCont_mod)
 summary(ACE_LinTrendZeroCont_mod)
 
 #quick view of the data, for the age variable 
 plot(age_mod)
 #Perform Levene's Test for homogenity of variances 
-leveneTest(Age ~ Group, data = DPRC_demographics)
+leveneTest(Age ~ Group, data = your_file_name)
 #Perform a Shapiro-Wilk test for normality of residuals
 shapiro.test(age_mod$residuals)
 
@@ -225,43 +218,42 @@ ggplot(df_circle_plot, aes(x = group_names, y = group_number)) +
 
 
 
-
 #######--------------- Longitudinal (F0 vs. F2) analysis ---------------########
 
 #read in excel files (participant data file)
-DPRC_demographics <- read.csv("your_data_file_longitudinal.csv") #longitudinal
-#e.g., DPRC_demographics <- read.csv("longitudinal_DPRC_neuropsych_data_lined_up_valid_participants.csv") #longitudinal
+your_file_name <- read.csv("your_data_file_longitudinal.csv") #longitudinal
+#e.g., your_file_name <- read.csv("longitudinal_DPRC_neuropsych_data_lined_up_valid_participants.csv") #longitudinal
 
 #rename first column
-colnames(DPRC_demographics)[1] <-'ParticipantID'
+colnames(your_file_name)[1] <-'ParticipantID'
 
 #convert categorical variables to a factor
-DPRC_demographics$Group <- as.factor(DPRC_demographics$Group)
-DPRC_demographics$Sex_binary <- as.factor(DPRC_demographics$Sex_binary)
-DPRC_demographics$Timepoint <- as.factor(DPRC_demographics$Timepoint)
+your_file_name$Group <- as.factor(your_file_name$Group)
+your_file_name$Sex_binary <- as.factor(your_file_name$Sex_binary)
+your_file_name$Timepoint <- as.factor(your_file_name$Timepoint)
 
 #convert variables to numeric 
-DPRC_demographics$Age<- as.numeric(DPRC_demographics$Age)
-DPRC_demographics$ACE<- as.numeric(DPRC_demographics$ACE)
+your_file_name$Age<- as.numeric(your_file_name$Age)
+your_file_name$ACE<- as.numeric(your_file_name$ACE)
 
 #look at descriptive statistics
-age_descrip <- describeBy(DPRC_demographics$Age, list(DPRC_demographics$Group, DPRC_demographics$Timepoint))
-ACE_descrip <- describeBy(DPRC_demographics$ACE, list(DPRC_demographics$Group, DPRC_demographics$Timepoint))
-gender_descrip <- by(DPRC_demographics$Group, list(DPRC_demographics$Sex, DPRC_demographics$Timepoint), summary)
-clinsite_descrip <- by(DPRC_demographics$Group, list(DPRC_demographics$Clinical_site, DPRC_demographics$Timepoint), summary)
+age_descrip <- describeBy(your_file_name$Age, list(your_file_name$Group, your_file_name$Timepoint))
+ACE_descrip <- describeBy(your_file_name$ACE, list(your_file_name$Group, your_file_name$Timepoint))
+gender_descrip <- by(your_file_name$Group, list(your_file_name$Sex, your_file_name$Timepoint), summary)
+clinsite_descrip <- by(your_file_name$Group, list(your_file_name$Clinical_site, your_file_name$Timepoint), summary)
 
 ####---------------------plot & analyse the data to visualise-------------------------------####
 
 #take a subset of the DPRC data - only baseline data (F0) to display in graphs
-baseline_DPRC_demographics <- DPRC_demographics[ which(DPRC_demographics$Timepoint=='F0'), ]
+baseline_your_file_name <- your_file_name[ which(your_file_name$Timepoint=='F0'), ]
 
 #whole sample descriptives
 #ACE
-mean(baseline_DPRC_demographics$ACE)
-sd(baseline_DPRC_demographics$ACE)
+mean(baseline_your_file_name$ACE)
+sd(baseline_your_file_name$ACE)
 
 #plot ACE (violin plot) - for F0, baseline, example
-ggplot(baseline_DPRC_demographics, aes(x = Group, y = ACE)) + 
+ggplot(baseline_your_file_name, aes(x = Group, y = ACE)) + 
     geom_boxplot(width = 0.1, fill = "white", outlier.size = 1, aes(colour = Group)) + 
     stat_summary(fun = mean, geom = "point", shape = 19, size = 2, aes(colour = Group)) + 
     xlab("Group") + 
@@ -271,7 +263,7 @@ ggplot(baseline_DPRC_demographics, aes(x = Group, y = ACE)) +
     theme(legend.position = "none") +
     geom_violin(trim = FALSE, alpha = .5, aes(fill = Group, colour = Group), size = 1)
 #plot ACE (violin plot) - for F0 vs. F2, longitudinal example
-ggplot(DPRC_demographics, aes(x = Group, y = ACE, fill = Timepoint)) + 
+ggplot(your_file_name, aes(x = Group, y = ACE, fill = Timepoint)) + 
     geom_boxplot(width = 0.1, fill = "white", outlier.size = 1, aes(colour = Timepoint), position = position_dodge(.9)) + 
     stat_summary(fun = mean, geom = "point", shape = 19, size = 2, aes(colour = Timepoint), position = position_dodge(.9)) + 
     xlab("Group") + 
@@ -280,7 +272,7 @@ ggplot(DPRC_demographics, aes(x = Group, y = ACE, fill = Timepoint)) +
     theme_classic() +
     geom_violin(trim = FALSE, alpha = .5, aes(fill = Timepoint, colour = Timepoint), size = 1)
 #colour by group
-ggplot(DPRC_demographics, aes(x = Group, y = ACE, group=interaction(Group, Timepoint))) + 
+ggplot(your_file_name, aes(x = Group, y = ACE, group=interaction(Group, Timepoint))) + 
     geom_boxplot(width = 0.1, fill = "white", outlier.size = 1, aes(colour = Group), position = position_dodge(.9)) + 
     stat_summary(fun = mean, geom = "point", shape = 19, size = 2, aes(colour = Group), position = position_dodge(.9)) + 
     xlab("Group") + 
@@ -290,7 +282,7 @@ ggplot(DPRC_demographics, aes(x = Group, y = ACE, group=interaction(Group, Timep
     theme(legend.position = "none") +
     geom_violin(trim = FALSE, alpha = .5, aes(fill = Group, colour = Group), size = 1)
 #plot ACE (raincloud plot) - for F0
-ggplot(baseline_DPRC_demographics, aes(x = Group, y = ACE, fill = Group)) + 
+ggplot(baseline_your_file_name, aes(x = Group, y = ACE, fill = Group)) + 
     geom_flat_violin(position = position_nudge(x = .2, y = 0), alpha = .8) +
     geom_point(aes(y = ACE, color = Group), position = position_jitter(width = .15), size = .5, alpha = 0.8) +
     geom_boxplot(width = 0.1, fill = "white", outlier.size = 1, aes(colour = Group)) + 
@@ -302,7 +294,7 @@ ggplot(baseline_DPRC_demographics, aes(x = Group, y = ACE, fill = Group)) +
     theme(legend.position = "none") +
     coord_flip()
 #plot ACE (raincloud plot) - for F0 vs.F2
-ggplot(DPRC_demographics, aes(x = Group, y = ACE, fill = Timepoint)) + 
+ggplot(your_file_name, aes(x = Group, y = ACE, fill = Timepoint)) + 
     geom_flat_violin(position = position_nudge(x = .2, y = 0), alpha = .8) +
     geom_point(aes(y = ACE, color = Timepoint), position = position_jitter(width = .15), size = .5, alpha = 0.8) +
     geom_boxplot(width = 0.1, fill = "white", outlier.size = 1, aes(colour = Timepoint)) + 
@@ -313,7 +305,7 @@ ggplot(DPRC_demographics, aes(x = Group, y = ACE, fill = Timepoint)) +
     theme_classic() +
     coord_flip()
 #colour by group
-ggplot(DPRC_demographics, aes(x = Group, y = ACE, group=interaction(Group, Timepoint))) + 
+ggplot(your_file_name, aes(x = Group, y = ACE, group=interaction(Group, Timepoint))) + 
     geom_flat_violin(position = position_nudge(x = .2, y = 0), alpha = .8) +
     geom_point(aes(y = ACE, color = Group), position = position_jitter(width = .15), size = .5, alpha = 0.8) +
     geom_boxplot(width = 0.1, fill = "white", outlier.size = 1, aes(colour = Group)) + 
@@ -325,12 +317,12 @@ ggplot(DPRC_demographics, aes(x = Group, y = ACE, group=interaction(Group, Timep
     theme_classic() +
     coord_flip()
 
-#check for significant difference in age between groups 
-age_mod <- lm(Age ~ Classification, data = baseline_DPRC_demographics)
-#age_mod <- lm(Age ~ 0 + Classification, data = DPRC_demographics) #test against y-intercept
+#check for significant difference in age between groups, as an example
+age_mod <- lm(Age ~ Classification, data = baseline_your_file_name)
+#age_mod <- lm(Age ~ 0 + Classification, data = your_file_name) #test against y-intercept
 anova(age_mod)
 #Bayesian (put into a new dataset b/c can't have any NA values)
-For_Bay_data <- dplyr::select(baseline_DPRC_demographics, ParticipantID, Group, Age, ACE)
+For_Bay_data <- dplyr::select(baseline_your_file_name, ParticipantID, Group, Age, ACE)
 summary(For_Bay_data)
 For_Bay_data_noNas <- na.omit(For_Bay_data)
 anovaBF(Age ~ Group, data = For_Bay_data_noNas) 
@@ -341,7 +333,6 @@ etaSquared(age_mod)
 age_group_means <- c(age_descrip$`1`$mean, age_descrip$`2`$mean, age_descrip$`3`$mean, age_descrip$`4`$mean, age_descrip$`5`$mean)
 power_age_n <- power.anova.test(groups = length(age_group_means), between.var = anova(age_mod)$`Sum Sq`[1], within.var = anova(age_mod)$`Sum Sq`[2], power = .8, sig.level = 0.05)
 power_age_power<- power.anova.test(groups = length(ACE_group_means), between.var = anova(age_mod)$`Sum Sq`[1], within.var = anova(age_mod)$`Sum Sq`[2], n = 41, sig.level = 0.05)
-
 
 #check for significant difference in gender between groups 
 #reformat data for chi-square test
@@ -357,64 +348,4 @@ gender_chi_test
 contingencyTableBF(gender_data_chisq, sampleType = "jointMulti")
 #a good resource on running Bayesian chi-squared tests: https://stats.libretexts.org/Bookshelves/Applied_Statistics/Book%3A_Learning_Statistics_with_R_-_A_tutorial_for_Psychology_Students_and_other_Beginners_(Navarro)/17%3A_Bayesian_Statistics/17.06%3A_Bayesian_Analysis_of_Contingency_Tables
 cramersV(gender_data_chisq)
-
-
-#check for significant difference in clinical site between groups 
-#reformat data for chi-square test
-#location_data_chisq <- rbind(c(21,36,21,18,10), c(1,3,4,3,0), c(0,1,5,0,1))
-#for dMRI-fMRI study (n = 119)
-location_data_chisq <- rbind(c(21,36,21,16,9), c(0,3,4,3,0), c(0,1,4,0,1))
-colnames(location_data_chisq) <- c("C", "SCD", "aMCI", "mMCI", "AD")
-rownames(location_data_chisq) <- c("Auckland", "Christchurch", "Dunedin")
-#run chi-square test
-location_chi_test <- chisq.test(location_data_chisq)
-location_chi_test
-contingencyTableBF(location_data_chisq, sampleType = "jointMulti")
-cramersV(location_data_chisq)
-#if expected values in cells are too small, simulate more p-values:
-#location_chi_test <- chisq.test(location_data_chisq, simulate.p.value = TRUE)
-
-
-#check for significant difference in ACE between groups 
-ACE_mod <- lm(ACE ~ Group, data = baseline_DPRC_demographics)
-anova(ACE_mod)
-anovaBF(ACE ~ Group, data = For_Bay_data_noNas) 
-etaSquared(ACE_mod)
-#whole sample - ACE descriptives
-mean(For_Bay_data_noNas$ACE)
-sd(For_Bay_data_noNas$ACE)
-
-
-#conduct power analysis for ACE
-ACE_group_means <- c(ACE_descrip$`1`$mean, ACE_descrip$`2`$mean, ACE_descrip$`3`$mean, ACE_descrip$`4`$mean, ACE_descrip$`5`$mean)
-#power_ACE <- power.anova.test(groups = length(ACE_group_means), between.var = var(ACE_group_means), within.var = 7850.6, power = .8, sig.level = 0.05)
-power_ACE_n <- power.anova.test(groups = length(ACE_group_means), between.var = anova(ACE_mod)$`Sum Sq`[1], within.var = anova(ACE_mod)$`Sum Sq`[2], power = .8, sig.level = 0.05)
-power_ACE_power<- power.anova.test(groups = length(ACE_group_means), between.var = anova(ACE_mod)$`Sum Sq`[1], within.var = anova(ACE_mod)$`Sum Sq`[2], n = 41, sig.level = 0.05)
-
-
-
-#quick view of the data
-plot(age_mod)
-
-
-#Perform Levene's Test for homogenity of variances
-leveneTest(Age ~ Group, data = baseline_DPRC_demographics)
-
-
-#Perform a Shapiro-Wilk test for normality of residuals
-shapiro.test(age_mod$residuals)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
