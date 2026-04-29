7677f9e5ab0a09b3ec9076d1cc7a723cd5abbd41
Lenore <52559898+Ltah72@users.noreply.github.com>
2026-04-30 10:35:57 +1200

    Update RunFIX.m
    Making it more reproducible/translatable for public usage

--- fMRI/RunFIX.m
+++ fMRI/RunFIX.m
@@ -3,7 +3,6 @@
 %files. 
 
 
-
 clc;
 clear all;
 close all;
@@ -12,21 +11,20 @@ close all;
 period = input('Which time period do you want to analyse, e.g. F0, F2, all, etc?: ', 's');
 
 %Define fmriprep directory:
-%FmriprepDirectory = input('Please enter fmriprep directory:', 's');
-FmriprepDir = '/data/USERS/LENORE/fmriprep_test/';
+FmriprepDir = input('Please enter fmriprep directory:', 's');
+%e.g., FmriprepDir = '/data/USERS/LENORE/fmriprep_directory/';
 
 %Script directory is defined, so that it can be added to path below:
-%ScriptDirectory = input('Please enter script directory:', 's');
-ScriptDirectory = '/data/USERS/LENORE/scripts/dprc/';
+ScriptDirectory = input('Please enter script directory:', 's');
+%e.g., ScriptDirectory = '/data/USERS/LENORE/scripts/dprc/';
 
 %Add your script and all necessary files (e.g. data, functions) to path
 addpath(genpath(FmriprepDir));
 addpath(genpath(ScriptDirectory));
 
 %specify directory where hand label noise text files are
-%FmriprepDirectory = input('Please enter directory that holds your manually classified ICA labels:', 's');
-HandLabelsICA_dir = '/data/USERS/LENORE/MELODIC/hand_labels_text_files/';
-
+HandLabelsICA_dir = input('Please enter directory that holds your manually classified ICA labels:', 's');
+%e.g., HandLabelsICA_dir = '/data/USERS/LENORE/MELODIC/hand_labels_text_files/';
 
 %go into fmriprep derivatives directory
 cd([FmriprepDir 'derivatives/fmriprep/']);
@@ -34,24 +32,20 @@ cd([FmriprepDir 'derivatives/fmriprep/']);
 %choose participants to train FIX on
 FIX_train_PAR = uipickfiles;
 
-%choose participants to run the trained FIX on
-%participants = uipickfiles;
-
 %copy the necessary preprocessed files from fmriprep into a new folder (named for_FIX.ica)
-for i = 10:length(FIX_train_PAR)
+for i = 1:length(FIX_train_PAR)
     
     [upper_path, PAR_NAME, ~] = fileparts(FIX_train_PAR{1,i});
 
     %copy, rename, & move hand label noise text files into each of the participant's FIX files.
     copyfile([HandLabelsICA_dir, 'labels_' PAR_NAME, '.txt'], ['hand_labels_noise.txt']);
     movefile(['hand_labels_noise.txt'], [FmriprepDir 'derivatives/fmriprep/' PAR_NAME '/func/for_FIX.ica']);
-
 end 
 
 
 %run FIX - the output weighted file will be in the fix training files
 %directory. Note that you need to list out your participants and their 
-%specific directories here. 
+%specific directories here. (INCOMPLETE)
 unix(['/SOFTWARE/fix/fix -t /SOFTWARE/fix/training_files/TrainingDPRC.RData -l ' FmriprepDir, 'derivatives/fmriprep/sub-ADPRC0001F0/func/for_FIX.ica ' FmriprepDir, 'derivatives/fmriprep/sub-ADPRC0019F0/func/for_FIX.ica ' FmriprepDir, 'derivatives/fmriprep/sub-ADPRC0020F0/func/for_FIX.ica ' FmriprepDir, 'derivatives/fmriprep/sub-ADPRC0057F0/func/for_FIX.ica ' FmriprepDir, 'derivatives/fmriprep/sub-ADPRC0069F0/func/for_FIX.ica ' FmriprepDir, 'derivatives/fmriprep/sub-CDPRC0034F0/func/for_FIX.ica ' FmriprepDir, 'derivatives/fmriprep/sub-CDPRC0036F0/func/for_FIX.ica ' FmriprepDir, 'derivatives/fmriprep/sub-CDPRC0041F0/func/for_FIX.ica ' FmriprepDir, 'derivatives/fmriprep/sub-DDPRC0012F0/func/for_FIX.ica ' FmriprepDir, 'derivatives/fmriprep/sub-DDPRC0013F0/func/for_FIX.ica ' FmriprepDir, 'derivatives/fmriprep/sub-DDPRC0022F0/func/for_FIX.ica']);
 unix(['/SOFTWARE/fix/fix -t /SOFTWARE/fix/training_files/TrainingDPRC.RData -l ' FmriprepDir, 'derivatives/fmriprep/sub-ADPRC0001F0/func/for_FIX.ica']);
 
